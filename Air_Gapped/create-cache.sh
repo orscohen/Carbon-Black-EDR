d4074e949f1cc979d375306b5bd853db7e2bc9c9
orscohen <136265306+orscohen@users.noreply.github.com>
2024-01-30 20:17:14 +0200

    Update create-cache.sh

--- Air_Gapped/create-cache.sh
+++ Air_Gapped/create-cache.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+#yum-utils
 # Create /tmp/cb/yum-utils directory if it doesn't exist
 mkdir -p /tmp/cb/yum-utils
 
 # Install yum-utils if not already installed
 if ! command -v yumdownloader &> /dev/null; then
 # Download yum-utils to /tmp/cb/yum-utils
+# Specify the download directory
 sudo yum install --downloadonly --downloaddir="/tmp/cb/yum-utils" yum-utils -y
-yum clean all
-sudo yum localinstall -y /tmp/cb/yum-utils/*.rpm
-
+rpm -ivh /tmp/cb/yum-utils/*.rpm
 fi
-
-# Create the cb temp directory if it doesn't exist
-mkdir -p /tmp/cb
+#createrepo
+# Create /tmp/cb/createrepo directory if it doesn't exist
+mkdir -p /tmp/cb/createrepo
+# Download createrepo to /tmp/cb/createrepo
+sudo yum install --downloadonly --downloaddir="/tmp/cb/createrepo" createrepo -y
 
 # CB Enterprise:
 # Download the main package to /tmp/cb with --nogpgcheck
@@ -23,10 +25,4 @@ sudo yumdownloader --resolve --destdir=/tmp/cb --nogpgcheck --archlist=x86_64 cb
 for dep in $(repoquery --requires --resolve --archlist=x86_64 cb-enterprise); do
     sudo yumdownloader --resolve --destdir=/tmp/cb --nogpgcheck --archlist=x86_64 "$dep"
 done
-# Create /tmp/cb/createrepo directory if it doesn't exist
-mkdir -p /tmp/cb/createrepo
-# Download createrepo to /tmp/cb/createrepo
-sudo yum install --downloadonly --downloaddir="/tmp/cb/createrepo" createrepo -y
-
-
 
