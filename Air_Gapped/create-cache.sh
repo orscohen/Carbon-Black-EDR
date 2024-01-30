ea74884da37c27b8c7baa815288f487bf7cdb438
orscohen <136265306+orscohen@users.noreply.github.com>
2024-01-30 19:41:19 +0200

    Update create-cache.sh

--- Air_Gapped/create-cache.sh
+++ Air_Gapped/create-cache.sh
@@ -2,12 +2,13 @@
 
 # Create /tmp/cb/yum-utils directory if it doesn't exist
 mkdir -p /tmp/cb/yum-utils
-# Download yum-utils to /tmp/cb/yum-utils
-sudo yum install --downloadonly --downloaddir="/tmp/cb/yum-utils" yum-utils -y
 
 # Install yum-utils if not already installed
 if ! command -v yumdownloader &> /dev/null; then
-    sudo yum install -y yum-utils
+# Download yum-utils to /tmp/cb/yum-utils
+sudo yum install --downloadonly --downloaddir="/tmp/cb/yum-utils" yum-utils -y
+sudo yum localinstall -y /tmp/cb/yum-utils/*.rpm
+
 fi
 
 # Create the cb temp directory if it doesn't exist
